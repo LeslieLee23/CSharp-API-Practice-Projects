61c2ddf7af911b9eca2b5287bde3201d4f07f5f2
Leslie Lee <flesslele@gmail.com>
2024-04-11 10:46:42 -0700

    Impelement the interface

--- WebApi/IKeyValueStore.cs
+++ WebApi/IKeyValueStore.cs
@@ -1,3 +1,5 @@
+using YamlDotNet.Core.Tokens;
+
 public interface IKeyValueStore
 {
     bool Create(string key, string value);
@@ -5,5 +7,41 @@ public interface IKeyValueStore
     bool Update(string key, string value);
     bool Delete(string key);
     IDictionary<string, string> GetAll();
+}
+
+public class KeyValueStore : IKeyValueStore
+{
+    private readonly Dictionary<string, string> _store = new();
+    public bool Create(string key, string value)
+    {
+        return _store.TryAdd(key, value);
+    }
+
+    public string Read(string key)
+    {
+        _store.TryGetValue(key, out var value);
+        return value;
+    }
+
+    public bool Update(string key, string value)
+    {
+        if (_store.ContainsKey(key))
+        {
+             _store[key] = value;
+             return true;
+        }
+        return false;
+    }
+
+    public bool Delete(string key)
+    {
+        return _store.Remove(key);
+    }
+
+    public IDictionary<string, string> GetAll()
+    {
+        return new Dictionary<string, string>(_store);
+    }
+
 
 }
